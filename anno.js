34d6272813de0316e66e0949e0eb1c1efff94569
Brian Hayes <brian@bit-player.org>
2016-03-20 16:16:26 -0400

    first commit on spanish branch

--- anno.js
+++ anno.js
@@ -1,49 +1,33 @@
 
 function setTriggers () {
-  var i;
+  var i, note;
   var triggers = document.getElementsByClassName('trigger');
-  var notes = document.getElementsByTagName('aside');
   console.log(triggers);
   for (i = 0; i < triggers.length; i++) {
     triggers[i].addEventListener('click', toggleNote, false);
-  }
-  for (i = 0; i < notes.length; i++) {
-    notes[i].addEventListener('click', toggleNote, false);
-  }
-  
+    note = triggers[i].nextSibling;
+    if (note.classList.contains('note')) {
+      note.addEventListener('click', toggleNote, false);
+    }
+  }  
 }
 
-function nextAside (node) {
-  var here = node;
-  console.log('in nextAside; here = ', here);
-  while (here && here.tagName != 'ASIDE') {
-    console.log('in while loop; here = ', here);
-    here = here.nextSibling();
-  }
-  console.log('done with while loop; here = ', here);
-  return here;
-}
 
 
 
 function toggleNote () {
   var note;
   console.log("clicked; this = ", this, this.tagName);
-  if (this.tagName === 'aside') {
+  if (this.classList.contains === 'note') {
     note = this;
     console.log('In toggleNote; note = ', note);
   }
   else {
-    note = nextAside(this);
+    note = this.nextSibling;
   }
   if (note.style.display === 'none') {
-    if (note.classList.contains('block')) {
-      note.style.display = 'block';
+    note.style.display = 'inline-block';
     }
-    else {
-      note.style.display = 'inline';
-    }
-  }
   else {
     note.style.display = 'none';
   }
